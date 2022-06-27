0bc85f2fe4071c03372b1c19bfdd6dd3e3a059f3
MenanAk47 <35970476+MenanAk47@users.noreply.github.com>
2022-06-27 20:09:43 +0600

    delay

--- config.lua
+++ config.lua
@@ -2,6 +2,7 @@ Config = {}
 Config.Locale = "en"
 Config.PatreonEmail = 'Your Patron Email'               --Take authorization with your patreon email.
 Config.SharedObjectName = 'esx:getSharedObject'         --Change if you Shared Object Name is different
+Config.FirstLoginCheckDelay = 10 -- 10 seconds
     
 Config.FullChecking = true 	-- 	Make a full check of player clothing after joining the server and after closing the skin menu. 
 							--	Makes clothing slot protection. Also, this can make performance issue for large amount of clothing as item.
